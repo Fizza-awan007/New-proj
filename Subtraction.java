93e295d51afd18a5c08bbf3e976dbcdafab32823
Fizza Awan <fizzaawan817@gmail.com>
2024-07-31 18:09:10 +0500

    subtraction of two numbers

---
+++ Subtraction.java
@@ -0,0 +1,15 @@
+
+public class Subtraction {
+    public static void main(String[] args) {
+        
+        int num1=9;
+        int num2=10;
+        // Perform subtraction
+        int result = num1 - num2;
+
+        // Display the result
+        System.out.println("The result of subtraction is: " + result);
+        
+       
+    }
+}
